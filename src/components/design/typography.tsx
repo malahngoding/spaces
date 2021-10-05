465b1a26bdac975c4259b52ca6692a9d3da142f5
malahngodingbot <malahngoding@gmail.com>
2021-10-05 14:18:03 +0700

    chore(typo): unselect title text

--- src/components/design/typography.tsx
+++ src/components/design/typography.tsx
@@ -4,21 +4,25 @@ export const Heading = styled(`h1`, {
   fontSize: `$xl`,
   fontWeight: 900,
   color: `$slate12`,
+  userSelect: `none`,
 });
 export const SubHeading = styled(`h2`, {
   fontSize: `$xl`,
   fontWeight: 700,
   color: `$slate12`,
+  userSelect: `none`,
 });
 export const Title = styled(`h3`, {
   fontSize: `$lg`,
   fontWeight: 900,
   color: `$slate12`,
+  userSelect: `none`,
 });
 export const SubTitle = styled(`h4`, {
   fontSize: `$md`,
   fontWeight: 700,
   color: `$slate12`,
+  userSelect: `none`,
 });
 export const Caption = styled(`h6`, {
   margin: 0,
@@ -26,6 +30,7 @@ export const Caption = styled(`h6`, {
   fontSize: `$xxs`,
   fontWeight: 300,
   color: `$slate12`,
+  userSelect: `none`,
 });
 export const Paragraph = styled(`p`, {
   fontSize: `$sm`,
