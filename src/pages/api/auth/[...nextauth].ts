82641f996c4759f85922f3b736437f35a594721e
malahngodingbot <malahngoding@gmail.com>
2021-10-14 07:29:22 +0700

    fix(auth): fixing breaking build code in auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProviders from 'next-auth/providers/credentials';
-import authService from '@services/auth-service';
 
 export default NextAuth({
   jwt: {
@@ -20,39 +18,6 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || ``,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ``,
-    }),
-    CredentialsProviders({
-      name: `Credentials`,
-      async authorize(req) {
-        try {
-          const { email } = req;
-          const { password } = req;
-          const response = await authService({
-            method: `post`,
-            url: `/api/signin`,
-            data: {
-              email,
-              password,
-            },
-          });
-
-          if (response.data.status === `SUCCESS`) {
-            return {
-              name: response.data.user.name,
-              email: response.data.user.email,
-              image: `https://avatars.dicebear.com/api/miniavs/${response.data.user.email}.svg`,
-            };
-          }
-          return {
-            name: ``,
-            email: ``,
-            image: `https://avatars.dicebear.com/api/miniavs/error.svg`,
-          };
-        } catch (e) {
-          const errorMessage = e.response.data.message;
-          throw new Error(`${errorMessage}&email=`);
-        }
-      },
     }),
   ],
 });
