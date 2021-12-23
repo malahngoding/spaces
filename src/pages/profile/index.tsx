2623c8d22a8793e01334f50a8d9c00f4720600d9
hecterbonha <untungsoandryanto@gmail.com>
2021-12-23 09:49:44 +0700

    feat(init): hustle

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,7 +6,7 @@ import { ProfileLayout } from '@layouts/profile';
 import { Section } from '@components/design/section';
 import { SubTitle } from '@components/design/typography';
 import { DetailsForm } from '@components/profile/details-form';
-import { MiscForm } from '@components/profile/misc.form';
+import { MiscForm } from '@components/profile/misc-form';
 
 interface ProfileProps {
   currentUser: {
