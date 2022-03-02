3694c4c3afe629011f32314239f1c5b182ae3740
hecterbonha <untungsoandryanto@gmail.com>
2022-03-02 10:21:24 +0700

    feat(hashconnect): init

--- src/utils/proto_procedure.ts
+++
@@ -1,9 +0,0 @@
-import { credentials } from '@grpc/grpc-js';
-import { GreeterClient } from '../../proto/procedure_grpc_pb';
-
-const port = 9000;
-
-export const clientGRPC = new GreeterClient(
-  `localhost:${port}`,
-  credentials.createInsecure(),
-);
