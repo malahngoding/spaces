c11c9627d7d7ab8ea2ff8a20d328f830e752982c
hecterbonha <untungsoandryanto@gmail.com>
2023-01-13 10:28:48 +0000

    ok

--- styles/global.css.ts
+++ styles/global.css.ts
@@ -1,5 +1,7 @@
 import { globalStyle } from '@vanilla-extract/css';
+import { vars } from './theme/index.css';
 
 globalStyle('html, body', {
   margin: 0,
+  backgroundColor: vars.bluePrint.color.slate1,
 });
