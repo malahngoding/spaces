e66da56fb436bcd1e434a344a01f39b6c78a0060
hecterbonha <untungsoandryanto@gmail.com>
2023-01-18 23:03:40 +0000

    update deps

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -2,8 +2,8 @@ import { expect, test } from '@playwright/test';
 import StatusCodes from 'http-status-codes';
 
 test.describe('Can view the homepage', () => {
-    test('Show 200 code homepage', async ({ page }) => {
-        const response = await page.goto('/');
-        expect(response?.status()).toBe(StatusCodes.OK);
-    });
+	test('Show 200 code homepage', async ({ page }) => {
+		const response = await page.goto('/');
+		expect(response?.status()).toBe(StatusCodes.OK);
+	});
 });
