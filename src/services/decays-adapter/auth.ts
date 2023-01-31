ca552b0a38838a108818c417b583139a46eaa592
hecterbonha <untungsoandryanto@gmail.com>
2023-01-31 15:42:09 +0000

    add

--- src/services/decays-adapter/auth.ts
+++ src/services/decays-adapter/auth.ts
@@ -1,13 +1,13 @@
 import { decays } from '.';
 
 interface PostConnectReq {
-    random: number;
+	random: number;
 }
 
 interface PostConnectRes {
-    token: string;
+	token: string;
 }
 export const postConnect = async (req: PostConnectReq): Promise<PostConnectRes> => {
-    const res = await decays.get(`/echo/${req}`);
-    return res as PostConnectRes;
+	const res = await decays.get(`/echo/${req}`);
+	return res as PostConnectRes;
 };
