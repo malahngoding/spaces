a4d29d4f66af874efdeb47323afebf8c8dc3d391
hecterbonha <untungsoandryanto@gmail.com>
2023-02-01 05:33:11 +0000

    ok

--- src/routes/(base)/experiments/[slug]/+page.ts
+++ src/routes/(base)/experiments/[slug]/+page.ts
@@ -3,16 +3,16 @@ import { breadAndButter } from '../data';
 import type { PageLoad } from './$types';
 
 export const load = (({ params }) => {
-    console.log(breadAndButter);
-    console.log(params.slug.replace('/experiments', ''));
-    const filter = breadAndButter.filter(
-        (item) => item.link.replace('/experiments', '') === params.slug
-    );
+	console.log(breadAndButter);
+	console.log(params.slug.replace('/experiments', ''));
+	const filter = breadAndButter.filter(
+		(item) => item.link.replace('/experiments', '') === params.slug
+	);
 
-    return {
-        title: 'Hello world!',
-        content: filter
-    };
+	return {
+		title: 'Hello world!',
+		content: filter
+	};
 
-    throw error(404, 'Not found');
+	throw error(404, 'Not found');
 }) satisfies PageLoad;
